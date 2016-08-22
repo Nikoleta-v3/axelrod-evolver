37e0e965302e4e3337d7438ea3735eb66c3acef0
Nikoleta Glynatsi <GlynatsiNE@cardiff.ac.uk>
2016-08-22 20:03:54 +0100

    Corrections :
    - axl to axelrod
    - imported libraries, such as numpy, random, networkx
    - before run the tournament check for disconnected node. Same code
      as the ones in the experiments

--- spatial_utils.py
+++ spatial_utils.py
@@ -1,11 +1,14 @@
 from __future__ import division
 import axelrod
+import networkx as nx
+import numpy
+import random
 
 import axelrod_utils
 
-strategies = [s() for s in axl.strategies]
+strategies = [s() for s in axelrod.strategies]
 
-def ranks_for(my_strategy_factory, iterations=200, max_size=50,
+def ranks_for(my_strategy_factory, iterations=200, max_size=3,
               strategies=strategies, eval_function=numpy.median):
     """
     Given a function that will return a strategy, calculate the evaluation
@@ -24,13 +27,20 @@ def ranks_for(my_strategy_factory, iterations=200, max_size=50,
     for parameter in range(10):
 
         p = parameter/10
-        axl.seed(parameter)
-        G = nx.binomial_graph(len(strategies), p)
-        edges = G.edges()
-        tournament = axl.SpatialTournament(sample_strategies, edges=edges)
-        results = tournament.play(processes=0)
+        axelrod.seed(parameter)
+        G = nx.binomial_graph(len(sample_strategies), p)
 
-        ranks.append(results.ranking[-1])
+        # check that all nodes are connected
+        connections = [len(c) for c in sorted(nx.connected_components(G), key=len,
+                                                                  reverse=True)]
+
+        if connections and (1 not in connections) :
+            edges = G.edges()
+
+            tournament = axelrod.SpatialTournament(sample_strategies, edges=edges)
+            results = tournament.play(processes=0)
+
+            ranks.append(results.ranking[-1])
     eval_rank = eval_function(ranks)
     return eval_rank
 
