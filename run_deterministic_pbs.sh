e804bbf3f11453fab447cb2418b41decd0be3dfe
Nikoleta Glynatsi <GlynatsiNE@cardiff.ac.uk>
2016-08-27 00:22:11 +0100

    .sh Files to run pbs all together.

---
+++ run_deterministic_pbs.sh
@@ -0,0 +1,12 @@
+qsub pbs_files/complete_median_long.pbs
+qsub pbs_files/complete_median_short.pbs
+qsub pbs_files/complete_min_long.pbs
+qsub pbs_files/complete_min_short.pbs
+
+qsub pbs_files/deterministic/deterministic_binomial.pbs
+qsub pbs_files/deterministic/deterministic_complete.pbs
+qsub pbs_files/deterministic/deterministic_small.pbs
+qsub pbs_files/deterministic/deterministic_random.pbs
+
+
+
---
+++ run_pbs.sh
@@ -0,0 +1,22 @@
+qsub pbs_files/binomial_median_long.pbs
+qsub pbs_files/binomial_median_short.pbs
+qsub pbs_files/binomial_min_long.pbs
+qsub pbs_files/binomial_min_short.pbs
+
+qsub pbs_files/complete_median_long.pbs
+qsub pbs_files/complete_median_short.pbs
+qsub pbs_files/complete_min_long.pbs
+qsub pbs_files/complete_min_short.pbs
+
+qsub pbs_files/random_median_long.pbs
+qsub pbs_files/random_median_short.pbs
+qsub pbs_files/random_min_long.pbs
+qsub pbs_files/random_min_short.pbs
+
+qsub pbs_files/small_median_long.pbs
+qsub pbs_files/small_median_short.pbs
+qsub pbs_files/small_min_long.pbs
+qsub pbs_files/small_min_short.pbs
+
+
+
